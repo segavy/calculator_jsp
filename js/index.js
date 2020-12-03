dba8b099655a59ec795ec5fe8d5c00cfa48c04aa
61995113 <segavy@mail.ru>
2020-12-03 10:34:11 +0300

    final1

--- js/index.js
+++ js/index.js
@@ -7,35 +7,38 @@ var buttonDevide = document.getElementById('buttonDevide');
 function getInput1() {
     var input1 = document.getElementById('number1');
     var number1 = Number(input1.value);
+    return number1;
 }
 
 function getInput2 () {
     var input2 = document.getElementById('number2');
     var number2 = Number(input2.value);
+    return number2;
 }
 
 function onButtonPlusClick() {
     console.log('onButtonPlusClick');
-    //var input1 = document.getElementById('number1');
-    //var input2 = document.getElementById('number2');
-    //var number1 = Number(input1.value);
-    //var number2 = Number(input2.value);
     var summ = getInput1() + getInput2();
-    //var summ = number1+number2;
     window.alert(summ);
     console.log(summ);
 }
 
 function onButtonMinusClick() {
     console.log('onButtonMinusClick');
+    var min = getInput1() - getInput2();
+    window.alert(min);
 }
 
 function onButtonMultipleClick() {
     console.log('onButtonMultipleClick');
+    var mult = getInput1() * getInput2();
+    window.alert(mult);
 }
 
 function onButtonDevideClick() {
     console.log('onButtonDevideClick');
+    var dev = getInput1() / getInput2();
+    window.alert(dev);
 }
 
 buttonPlus.addEventListener('click', onButtonPlusClick);
