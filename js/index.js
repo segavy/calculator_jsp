408b5c3e06effca2813254e7ead65975cb46001c
61995113 <segavy@mail.ru>
2020-12-12 18:37:52 +0300

    update

--- js/index.js
+++ js/index.js
@@ -6,44 +6,50 @@ var buttonDevide = document.getElementById('buttonDevide');
 
 function getInput1() {
     var input1 = document.getElementById('number1');
-    var number1 = Number(input1.value);
-    return number1;
+    return Number(input1.value);
 }
 
 function getInput2 () {
     var input2 = document.getElementById('number2');
-    var number2 = Number(input2.value);
-    return number2;
+    return Number(input2.value);
 }
 
+function onButtonOperate(operateSymbol) {
+    if (operateSymbol === '+') {
+        var rezult=getInput1() + getInput2();
+    } else
+    if (operateSymbol === '-') {
+        var rezult = getInput1()-getInput2();
+    } else
+    if (operateSymbol === '*') {
+        var rezult = getInput1()*getInput2();
+    } else
+    if (operateSymbol === '/') {
+        var rezult = getInput1()/getInput2();
+    }
+    window.alert(rezult);
+}
+
+
 function onButtonPlusClick() {
-    console.log('onButtonPlusClick');
-    var summ = getInput1() + getInput2();
-    window.alert(summ);
-    console.log(summ);
+    onButtonOperate('+');
 }
 
 function onButtonMinusClick() {
-    console.log('onButtonMinusClick');
-    var min = getInput1() - getInput2();
-    window.alert(min);
+    onButtonOperate('-');
 }
 
 function onButtonMultipleClick() {
-    console.log('onButtonMultipleClick');
-    var mult = getInput1() * getInput2();
-    window.alert(mult);
+    onButtonOperate('*');
 }
 
 function onButtonDevideClick() {
-    console.log('onButtonDevideClick');
-    var dev = getInput1() / getInput2();
-    window.alert(dev);
+    onButtonOperate('/');
 }
+
 
 buttonPlus.addEventListener('click', onButtonPlusClick);
 buttonMinus.addEventListener('click', onButtonMinusClick);
 buttonMultiply.addEventListener('click', onButtonMultipleClick);
 buttonDevide.addEventListener('click', onButtonDevideClick);
-
 
